cb3ec56708e270e909bfff27c742d715600c777e
okit-man <diopamoses@gmail.com>
2026-09-23 05:27:54 +0100

    added all javascript functionality to enable both button works to ensure an effective slider

---
+++ data.js
@@ -0,0 +1,38 @@
+const reviewers = [
+  {
+    id: 1,
+    name: "Alice jackson",
+    profession: "product manager",
+    text:
+      "An oriented Product Manager with strong leadership and communication skills.They excel at defining product vision, aligning stakeholders, and prioritizing features that drive business growth.Their ability to balance user needs, technical feasibility, and business goals ensures smooth project execution.They are strategic, collaborative, and always deliver with clarity.",
+    img: "https://images.pexels.com/photos/3778603/pexels-photo-3778603.jpeg",
+  },
+  {
+    id: 2,
+    name: "joy chuchil",
+    profession: "frontend developer",
+    text:
+      "A talented Frontend Developer who writes clean, efficient, and responsive code. They have an eye for detail, ensuring that interfaces are pixel-perfect, accessible, and optimized for performance across devices. Their expertise in modern frameworks and problem-solving mindset helped accelerate development and improve user experience significantly.",
+    img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg",
+  },
+
+  {
+    id: 3,
+    name: "oluba macron",
+    profession: "backend developer",
+    text:
+      "Macron is a highly skilled Backend Developer who delivers scalable, secure, and reliable server-side solutions. They excel in designing APIs, managing databases, and ensuring seamless integration with frontend systems. Their strong debugging skills and focus on performance optimization made the overall system robust and efficient. A dependable team player.",
+    img: "https://images.pexels.com/photos/936090/pexels-photo-936090.jpeg",
+  },
+
+  {
+    id: 4,
+    name: "zik zikla",
+    profession: "mobile app developer",
+    text:
+      "Zik is a resourceful Mobile App Developer with expertise in building smooth, responsive, and user-friendly mobile applications. They understand platform-specific guidelines (iOS/Android) and deliver apps with excellent performance and clean architecture. Their attention to detail and passion for creating seamless mobile experiences made a huge impact on the project.",
+    img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg",
+  },
+];
+
+export default reviewers;
---
+++ index.js
@@ -0,0 +1,72 @@
+import people from  "./data.js"
+
+const container = document.querySelector(".slider-container");
+const prvBtn = document.querySelector(".prv-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+
+container.innerHTML = people.map((data, slideIndex) => {
+    // destructuring our data
+    const { name, profession, text, img } = data;
+    
+    // slider
+    let position = "next"
+    if (slideIndex === 0) {
+        position = 'active'
+    }
+    if (slideIndex === people.length - 1) {
+        position = 'last'
+    }
+    return ` <article class="slide ${position}" >
+    <img src="${img}" class="img" alt="${name}">
+                <h3>${name}</h3>
+                <p class="title">${profession}</p>
+                <p class="text">${text}</p>
+                <div class="quote-icon">
+                    <i class="fa-sharp fa-solid fa-quote-right"></i>
+                    </div>
+            </article>`;
+}).join('');
+
+
+const startSlider = (type) => {
+    // get all classes
+    const active = document.querySelector(".active");
+    const last = document.querySelector(".last");
+    let next = active.nextElementSibling;
+    if (!next) {
+        next = container.firstElementChild
+    };
+    
+    // remove all classes
+    active.classList.remove(["active"]);
+    last.classList.remove(["last"]);
+    next.classList.remove(["next"]);
+
+    if (type === 'prev') {
+        active.classList.add("next");
+        last.classList.add("active")
+        next = last.previousElementSibling
+        if (!next) {
+            next = container.lastElementChild
+        }
+        next.classList.remove(["next"])
+        next.classList.add("last")
+        return
+    };
+
+    // add all new classes to each array
+    active.classList.add("last");
+    last.classList.add("next");
+    next.classList.add("active");
+};
+
+nextBtn.addEventListener("click", () => {
+  startSlider();
+  
+});
+
+prvBtn.addEventListener("click", () => {
+  startSlider('prev');
+  
+});
